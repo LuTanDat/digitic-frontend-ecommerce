39a6aa02fca70fd9c5b3f1afcaf0d999ea2dde82
LuTanDat <ludathoc@gmail.com>
2023-11-18 16:50:21 +0700

    updates-order

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -84,88 +84,100 @@ const Orders = () => {
               orderState && orderState?.map((item, index) => {
                 if (selectedNavItem !== null && item?.orderStatus === selectedNavItem) {
                   return (
-                    <div style={{ backgroundColor: "#febd69" }} className='row mb-3 px-2' key={index}>
-                      <div className='col p-2 d-flex align-items-center gap-1'>
-                        <p>Mã đơn hàng: </p>
-                        <p>{item?._id}</p>
-                      </div>
-                      <div className='col p-2 d-flex align-items-center gap-1'>
-                        <p>Ngày đặt hàng: </p>
-                        <p>{new Date(item?.createdAt).toLocaleString()}</p>
-                      </div>
-                      <div className='col p-2 d-flex align-items-center gap-1'>
-                        <p className='mb-0'>Tổng tiền:</p>
-                        <p className='mb-0'>
-                          {item?.totalPrice ? (item?.totalPrice).toLocaleString("vi-VN", { style: "currency", currency: "VND" }) : "0 đ"}
-                        </p>
-                      </div>
-                      <div className='col-1 p-2 d-flex align-items-center gap-1'>
-                        <Link to={`/orders/${item?._id}`}>
-                          Xem chi tiết
-                        </Link>
-                      </div>
-                      <div className='col p-2 d-flex align-items-center gap-1'>
-                        <p className='mb-0'>Thanh toán:</p>
-                        <p className='mb-0'>{item?.paymentMethod}</p>
-                      </div>
-                      <div className='col p-2 d-flex gap-1 align-items-center justify-content-between'>
-                        <p className='mb-0'>Trạng thái: </p>
-                        <p className='mb-0' style={{ fontWeight: item?.orderStatus === "Đã Hủy" ? "600" : "" }}>{item?.orderStatus}</p>
-                        {
-                          item?.orderStatus !== "Đã Hủy" &&
-                          <button
-                            className='p-1'
-                            style={{ border: "1px solid #9255FD", borderRadius: "4px", color: "red" }}
-                            onClick={() => { dispatch(cancelOrder(item)) }}
-                          >
-                            Hủy đơn hàng
-                          </button>
-                        }
-                      </div>
+                    <div style={{ backgroundColor: "#febd69", borderRadius: "8px" }} className='row mb-3 p-2 text-center' key={index}>
+                      <table>
+                        <thead>
+                          <tr>
+                            <th>Mã đơn hàng</th>
+                            <th>Sản phẩm</th>
+                            <th>Phương thức thanh toán</th>
+                            <th>Tổng tiền</th>
+                            <th>Ngày đặt hàng</th>
+                            <th>Trạng thái</th>
+                            <th>Thao tác</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td className='py-2'>{item?._id}</td>
+                            <td className='py-2'>
+                              <Link to={`/orders/${item?._id}`}>
+                                Xem chi tiết
+                              </Link>
+                            </td>
+                            <td className='py-2'>{item?.paymentMethod}</td>
+                            <td className='py-2'>
+                              {item?.totalPrice ? (item?.totalPrice).toLocaleString("vi-VN", { style: "currency", currency: "VND" }) : "0 đ"}
+                            </td>
+                            <td className='py-2'>{new Date(item?.createdAt).toLocaleString()}</td>
+                            <td className='py-2'>
+                              <p className='mb-0' style={{ fontWeight: item?.orderStatus === "Đã Hủy" ? "550" : "" }}>{item?.orderStatus}</p>
+                            </td>
+                            <td className='py-2' style={{ width: "12%" }}>
+                              {
+                                item?.orderStatus !== "Đã Hủy" &&
+                                <button
+                                  className='p-1'
+                                  style={{ border: "1px solid #9255FD", borderRadius: "4px", color: "red" }}
+                                  onClick={() => { dispatch(cancelOrder(item)) }}
+                                >
+                                  Hủy đơn hàng
+                                </button>
+                              }
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
                     </div>
                   )
                 }
                 else
                   if (selectedNavItem === null) {
                     return (
-                      <div style={{ backgroundColor: "#febd69" }} className='row mb-3 px-2' key={index}>
-                        <div className='col p-2 d-flex align-items-center gap-1'>
-                          <p>Mã đơn hàng: </p>
-                          <p>{item?._id}</p>
-                        </div>
-                        <div className='col p-2 d-flex align-items-center gap-1'>
-                          <p>Ngày đặt hàng: </p>
-                          <p>{new Date(item?.createdAt).toLocaleString()}</p>
-                        </div>
-                        <div className='col p-2 d-flex align-items-center gap-1'>
-                          <p className='mb-0'>Tổng tiền:</p>
-                          <p className='mb-0'>
-                            {item?.totalPrice ? (item?.totalPrice).toLocaleString("vi-VN", { style: "currency", currency: "VND" }) : "0 đ"}
-                          </p>
-                        </div>
-                        <div className='col-1 p-2 d-flex align-items-center gap-1'>
-                          <Link to={`/orders/${item?._id}`}>
-                            Xem chi tiết
-                          </Link>
-                        </div>
-                        <div className='col p-2 d-flex align-items-center gap-1'>
-                          <p className='mb-0'>Thanh toán:</p>
-                          <p className='mb-0'>{item?.paymentMethod}</p>
-                        </div>
-                        <div className='col p-2 d-flex gap-1 align-items-center justify-content-between'>
-                          <p className='mb-0'>Trạng thái: </p>
-                          <p className='mb-0' style={{ fontWeight: item?.orderStatus === "Đã Hủy" ? "600" : "" }}>{item?.orderStatus}</p>
-                          {
-                            item?.orderStatus !== "Đã Hủy" &&
-                            <button
-                              className='p-1'
-                              style={{ border: "1px solid #9255FD", borderRadius: "4px", color: "red" }}
-                              onClick={() => { dispatch(cancelOrder(item)) }}
-                            >
-                              Hủy đơn hàng
-                            </button>
-                          }
-                        </div>
+                      <div style={{ backgroundColor: "#febd69", borderRadius: "8px" }} className='row mb-3 p-2 text-center' key={index}>
+                        <table>
+                          <thead>
+                            <tr>
+                              <th>Mã đơn hàng</th>
+                              <th>Sản phẩm</th>
+                              <th>Phương thức thanh toán</th>
+                              <th>Tổng tiền</th>
+                              <th>Ngày đặt hàng</th>
+                              <th>Trạng thái</th>
+                              <th>Thao tác</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td className='py-2'>{item?._id}</td>
+                              <td className='py-2'>
+                                <Link to={`/orders/${item?._id}`}>
+                                  Xem chi tiết
+                                </Link>
+                              </td>
+                              <td className='py-2'>{item?.paymentMethod}</td>
+                              <td className='py-2'>
+                                {item?.totalPrice ? (item?.totalPrice).toLocaleString("vi-VN", { style: "currency", currency: "VND" }) : "0 đ"}
+                              </td>
+                              <td className='py-2'>{new Date(item?.createdAt).toLocaleString()}</td>
+                              <td className='py-2'>
+                                <p className='mb-0' style={{ fontWeight: item?.orderStatus === "Đã Hủy" ? "550" : "" }}>{item?.orderStatus}</p>
+                              </td>
+                              <td className='py-2' style={{ width: "12%" }}>
+                                {
+                                  item?.orderStatus !== "Đã Hủy" &&
+                                  <button
+                                    className='p-1'
+                                    style={{ border: "1px solid #9255FD", borderRadius: "4px", color: "red" }}
+                                    onClick={() => { dispatch(cancelOrder(item)) }}
+                                  >
+                                    Hủy đơn hàng
+                                  </button>
+                                }
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
                       </div>
                     )
                   }
